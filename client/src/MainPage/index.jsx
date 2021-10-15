9a8d84efa025af847a98c3ad5466aaf151d89739
Matheus B <matheus_ob59@hotmail.com>
2021-10-14 22:22:39 -0300

    Create default (main page) component and base styles file.

---
+++ client/src/MainPage/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './styles.sass';
+
+function MainPage(){
+    return(
+        <div>
+            This is the main page.
+        </div>
+    );
+}
+
+export default MainPage;
