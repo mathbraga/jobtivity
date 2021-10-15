540653a3f892a62e61a5c4f8de510b5033c73665
Matheus B <matheus_ob59@hotmail.com>
2021-10-15 17:33:52 -0300

    Create dedicated directory for pages.

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import MainPage from "./MainPage";
+import MainPage from "./Pages/MainPage";
 
 function Routes(){
     return(
