fc12308ba9bde143c5ac6c84bbb96e67edd19293
Matheus B <matheus_ob59@hotmail.com>
2021-10-15 21:29:56 -0300

    Create components for page footer.

---
+++ client/src/components/FooterContent/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./styles.sass";
+
+function FooterContent(){
+    return(
+        <a className="footercontent" href="https://github.com/mathbraga" target="_blank">
+            <img src="./icons/GitHub-Mark-Light-32px.png"/>
+        </a>
+    )
+}
+
+export default FooterContent;
---
+++ client/src/components/MainPageFooter/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./styles.sass";
+
+import FooterContent from "../FooterContent";
+
+function MainPageFooter(){
+    return(
+        <div className="footer">
+            <FooterContent/>
+        </div>
+    )
+}
+
+export default MainPageFooter;
