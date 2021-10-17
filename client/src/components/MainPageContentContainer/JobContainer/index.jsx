b19cd698b6914eced3b9d501b3ac2fae35bcea67
Matheus B <matheus_ob59@hotmail.com>
2021-10-17 14:06:00 -0300

    Create job container component.

---
+++ client/src/components/MainPageContentContainer/JobContainer/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./styles.sass";
+
+function JobContainer(){
+    return(
+        <div className="job--container">
+            <span>Some job...</span>
+        </div>
+    )
+}
+
+export default JobContainer;
--- client/src/components/MainPageContentContainer/index.jsx
+++ client/src/components/MainPageContentContainer/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
+import JobContainer from "./JobContainer";
+
 import "./styles.sass";
 
 function MainPageContentContainer(){
     return(
         <div className="content--container">
-            <span>Jobtivity</span>
+            <JobContainer />
         </div>
     )
 }
