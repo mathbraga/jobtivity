9f5ac52e9d0e13d61ca645a73631b3b48c459db8
Matheus B <matheus_ob59@hotmail.com>
2021-10-26 20:08:56 -0300

    Add LinkedIn profile to footer.

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -6,14 +6,19 @@ import {
 } from "./styledComponents";
 
 import GithubIcon from "../../assets/Icons/GithubIcon.png";
+import LinkedinIcon from "../../assets/Icons/LinkedinIcon.png";
 
 const gitUrl = "https://github.com/mathbraga";
 const gitAltText = "Github Profile";
+
+const linkedinUrl = "https://www.linkedin.com/in/matheus-braga-730563149/";
+const linkedinAltText = "LinkedIn Profile";
 
 const Footer = () => {
     return(
         <FooterContainer>
             <FooterContent contentLink={gitUrl} iconUrl={GithubIcon} iconAlt={gitAltText} />
+            <FooterContent contentLink={linkedinUrl} iconUrl={LinkedinIcon} iconAlt={linkedinAltText} />
         </FooterContainer>
     );
 }
