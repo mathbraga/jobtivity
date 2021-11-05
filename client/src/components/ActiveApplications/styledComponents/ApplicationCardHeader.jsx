2a36c3e312db616d0a907b7322b684f581b936d7
Matheus B <matheus_ob59@hotmail.com>
2021-11-04 23:00:38 -0300

    Implement initial idea for application status selection.

--- client/src/components/ActiveApplications/styledComponents/ApplicationCardHeader.jsx
+++ client/src/components/ActiveApplications/styledComponents/ApplicationCardHeader.jsx
@@ -19,9 +19,12 @@ const ApplicationCardHeaderStyles = styled.div`
 
     .card--status_badge{
         display: flex;
-        justify-content: center;
+        position: relative;
+        justify-content: flex-start;
         align-items: center;
+    }
 
+    .card--status_button{
         width: max-content;
         height: auto;
         padding: 4px 8px;
@@ -30,6 +33,29 @@ const ApplicationCardHeaderStyles = styled.div`
         background-color: ${props => statusTypeToColor(props.status)};
         
         border-radius: 4px;
+        border: none;
+    }
+
+    .card--status_dropdown{
+        display: none;
+        position: relative;
+        width: 100%;
+    }
+
+    .card--status_dropdown div{
+        display: inline;
+        padding: 5px;
+        margin-left: 8px;
+
+        color: white;
+        background-color: ${props => statusTypeToColor(props.status)};
+        
+        border-radius: 4px;
+        border: none;
+    }
+
+    .card--status_badge:hover .card--status_dropdown{
+        display: inline-block;
     }
 `;
 
@@ -38,7 +64,12 @@ export const ApplicationCardHeader = (props) => {
         <ApplicationCardHeaderStyles status={props.status}>
             <div className="card--header">
                 <div className="card--status_badge">
-                    {props.status}
+                    <div className="card--status_button">{props.status}</div>
+                    <div className="card--status_dropdown">
+                        <div>1st</div>
+                        <div>2nd</div>
+                        <div>3rd</div>
+                    </div>
                 </div>
             </div>
         </ApplicationCardHeaderStyles>
