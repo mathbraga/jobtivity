558167b8b1e1419e73f6e5299b92349c92d462da
Matheus B <matheus_ob59@hotmail.com>
2021-12-08 22:30:36 -0300

    Create test for local storage creation function using jest.

---
+++ client/src/tests/globalHelpers.test.js
@@ -0,0 +1,10 @@
+import createEmptyStorage from "../globalHelperFunctions/initStorage";
+
+describe("Local storage", () => {
+    it("Is empty local storage created", () => {
+        const storageName = "applicationsList";
+        createEmptyStorage();
+
+        expect(localStorage.getItem(storageName)).toBe("[]");
+    });
+})
