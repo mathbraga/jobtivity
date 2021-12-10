5636f54bcc55d440d69270cea05a75acf9e49604
Matheus B <matheus_ob59@hotmail.com>
2021-12-09 23:11:19 -0300

    Create test for string to object conversion function.

--- client/src/tests/globalHelpers.test.js
+++ client/src/tests/globalHelpers.test.js
@@ -1,10 +1,23 @@
 import createEmptyStorage from "../globalHelperFunctions/initStorage";
 
+const storageName = "applicationsList";
+
+const convertStringToObject = (data) => JSON.parse(data);
+
 describe("Local storage", () => {
     it("Is empty local storage created", () => {
-        const storageName = "applicationsList";
         createEmptyStorage();
 
         expect(localStorage.getItem(storageName)).toEqual("[]");
+        localStorage.clear();
     });
-})
+});
+
+describe("Data manipulation for local storage", () => {
+    it("Convert string to object", () => {
+        const testData = '[{"test1": "a", "test2": "b"}]';
+        // const expectedTestResult = [{test1: 'a', test2: 'b'}];
+
+        expect(typeof convertStringToObject(testData)).toEqual('object');
+    });
+});
