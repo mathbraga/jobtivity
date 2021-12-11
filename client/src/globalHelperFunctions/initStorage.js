e62c20b7fb0e9777603140d1c75d2d266e443db0
Matheus B <matheus_ob59@hotmail.com>
2021-12-10 23:53:22 -0300

    Refactor code to use data conversion functions.

--- client/src/globalHelperFunctions/initStorage.js
+++ client/src/globalHelperFunctions/initStorage.js
@@ -1,10 +1,12 @@
+import { convertStringToObject } from "./dataConversionHelpers";
+
 const storageName = "applicationsList";
 
 const createEmptyStorage = () => {
     if(localStorage.getItem(storageName) === null)
       localStorage.setItem(storageName, "[]");
 
-    return JSON.parse(localStorage.getItem(storageName));
+    return convertStringToObject(localStorage.getItem(storageName));
 }
 
 export default createEmptyStorage;
