79393c80d106f14790dbc0ff4b3b2ac2ecd07708
Matheus B <matheus_ob59@hotmail.com>
2021-12-13 22:21:34 -0300

    Create tests for redux reducer functions.

---
+++ client/src/tests/reducers.test.js
@@ -0,0 +1,55 @@
+import reducer from "../store/reducers/applicationsReducer";
+
+describe("Global state manager reducers", () => {
+    it("Return applications object", () => {
+        expect(reducer(undefined, {})).toEqual({"applications": null});
+    });
+
+    it("Should update applications list", () => {
+        const testAction = {
+            type: "CHANGE_STATUS",
+            newStatus: "Awaiting",
+            index: 1
+        }
+        const testData = {
+            applications: 
+                [
+                    {
+                        name:"Jobtivity",
+                        date:"30/11/2021",
+                        website:"someurl.com",
+                        role:"FullStack",
+                        status:"Contact"
+                    },
+                    {
+                        name:"Amazon",
+                        date:"02/12/2021",
+                        website:"someurl.com",
+                        role:"FrontEnd",
+                        status:"Contact"
+                    }
+                ]
+        };
+        const expectedResult = {
+            applications: 
+                [
+                    {
+                        name:"Jobtivity",
+                        date:"30/11/2021",
+                        website:"someurl.com",
+                        role:"FullStack",
+                        status:"Contact"
+                    },
+                    {
+                        name:"Amazon",
+                        date:"02/12/2021",
+                        website:"someurl.com",
+                        role:"FrontEnd",
+                        status:"Awaiting"
+                    }
+                ]
+        }
+
+        expect(reducer(testData, testAction)).toEqual(expectedResult);
+    })
+});
