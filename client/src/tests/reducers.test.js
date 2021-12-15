2b71fb861643b9ee6e89671959c3529c10b9a902
Matheus B <matheus_ob59@hotmail.com>
2021-12-15 19:32:57 -0300

    Create test for aplication removal reducer.

--- client/src/tests/reducers.test.js
+++ client/src/tests/reducers.test.js
@@ -21,9 +21,17 @@ const sampleData = {
 };
 
 const temporaryReducer = (state, action) => {
-    const { applications } = state;
-    if(action.type == "ADD_APPLICATION"){
-        state.applications = [action.newApplication, ...applications];
+    const currentApplications = state.applications;
+    
+    if(action.type === "ADD_APPLICATION"){
+        state.applications = [action.newApplication, ...currentApplications];
+
+        return {...state};
+    }
+    else if(action.type === "DELETE_APPLICATION"){
+        currentApplications.splice(action.index, 1);
+        state.applications = [...currentApplications];
+
         return {...state};
     }
 
@@ -83,4 +91,22 @@ describe("Global state manager reducers", () => {
         expect(applications.length).toEqual(3);
         expect(latestApplication).toEqual(testAction.newApplication);
     });
+
+    it("Should delete designated application.", () => {
+        const testData = sampleData;
+        const testAction = {
+            type: "DELETE_APPLICATION",
+            index: 0
+        }
+        const { applications } = temporaryReducer(testData, testAction);
+        
+        expect(applications.length).toEqual(2);
+        expect(applications[0]).toEqual({
+            name:"Jobtivity",
+            date:"30/11/2021",
+            website:"someurl.com",
+            role:"FullStack",
+            status:"Contact"
+        })
+    })
 });
