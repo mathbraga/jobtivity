7a97b5234f592495f019efc91286947a764e2b1b
Matheus B <matheus_ob59@hotmail.com>
2021-12-21 23:02:28 -0300

    Style the add new application button.

--- client/src/components/PageTitle/styledComponents/NewApplicationButton.jsx
+++ client/src/components/PageTitle/styledComponents/NewApplicationButton.jsx
@@ -1,14 +1,34 @@
 import styled from "styled-components";
 
+import addIcon from "../../../assets/Icons/AddIconBlack.png";
+
 const ButtonStyles = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
     margin-left: auto;
 
     width: 60px;
     height: 20px;
+
+    border: none;
+    border-radius: 4px;
+    box-shadow: 0px 1px 2px #111;
+
+    background-color: var(--color-alternative);
+
+    cursor: pointer;
+
+    :active{
+        box-shadow: none;
+    }
 `;
 
 export const NewApplicationButton = (props) => {
     return(
-        <ButtonStyles onClick={() => console.log("clicked")}>{"+"}</ButtonStyles>
+        <ButtonStyles onClick={() => console.log("clicked")}>
+            <img src={addIcon} alt="Add item." />
+        </ButtonStyles>
     )
 }
