f9a46112dc4bcdb8e46008733351b39ca2a255e5
Matheus B <matheus_ob59@hotmail.com>
2021-12-24 22:35:24 -0300

    Create action for adding new application.

--- client/src/store/actions/updateStatus.js
+++ client/src/store/actions/updateStatus.js
@@ -6,6 +6,13 @@ export function updateStatus(newStatus, index){
     };
 }
 
+export function addApplication(newApplication){
+    return{
+        type: "ADD_APPLICATION",
+        newApplication: newApplication
+    }
+}
+
 export function deleteApplication(index){
     return{
         type: "DELETE_APPLICATION",
