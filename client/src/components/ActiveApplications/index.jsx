2b3e90e5b44ef2a535c464b758c31d4741f3402d
Matheus <matheusbraga59@outlook.com>
2022-01-17 21:51:43 -0300

    Create base implementation for search bar.

--- client/src/components/ActiveApplications/index.jsx
+++ client/src/components/ActiveApplications/index.jsx
@@ -5,7 +5,34 @@ import PageTitle from "../PageTitle";
 import { ApplicationCard } from "./styledComponents";
 import NewApplicationForm from "./styledComponents/NewApplicationForm";
 
+import styled from "styled-components";
+import SearchIcon from "../../assets/Icons/SearchIconWhite.png";
+
 const activeApplicationsPageTitle = "Active Applications";
+
+const SearchBar = styled.div`
+    width: 60%;
+    height: 40px;
+
+    border: 1px solid #ccc;
+
+    margin-bottom: 12px;
+
+    display: flex;
+    justify-content: start;
+    align-items: center;
+
+    img{
+        background-color: #ccc;
+        padding: 8px;
+    }
+
+    input{
+        width: 100%;
+        height: 100%;
+        border: 1px solid #ccc;
+    }
+`
 
 const ActiveApplications = (props) => {
     const { applicationsList, formState } = props;
@@ -20,6 +47,10 @@ const ActiveApplications = (props) => {
                 applicationCounter={numberOfApplications}
             />
             <NewApplicationForm isVisible={formState} />
+            <SearchBar>
+                <img src={SearchIcon} alt="Search Icon" />
+                <input type="text" />
+            </SearchBar>
             {applications.map((item, index) =>
                 <ApplicationCard {...item} key={index} index={index} />
             )}
