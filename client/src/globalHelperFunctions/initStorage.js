9d64d7737e84b96bb7d8596290bcdaa5ff27b218
Matheus B <matheus_ob59@hotmail.com>
2022-02-04 21:43:29 -0300

    Add history list to localStorage init function.

--- client/src/globalHelperFunctions/initStorage.js
+++ client/src/globalHelperFunctions/initStorage.js
@@ -1,8 +1,11 @@
 const storageName = "applicationsList";
+const historyStorageName = "applicationsHistory";
 
 const createEmptyStorage = () => {
     if(localStorage.getItem(storageName) === null)
       localStorage.setItem(storageName, "[]");
+    if(localStorage.getItem(historyStorageName) === null)
+      localStorage.setItem(historyStorageName, "[]");
 }
 
 export default createEmptyStorage;
