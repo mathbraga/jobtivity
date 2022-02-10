55ed6ac90ce2a556cc859f34b4627226ad45ef41
Matheus B <matheus_ob59@hotmail.com>
2022-02-10 15:11:21 -0300

    Add remote and location fields for application card.

--- client/src/components/Applications/styledComponents/ApplicationContentContainer.jsx
+++ client/src/components/Applications/styledComponents/ApplicationContentContainer.jsx
@@ -4,6 +4,8 @@ import companyIcon from "../../../assets/Icons/CompanyIcon.png";
 import roleIcon from "../../../assets/Icons/RoleIcon.png";
 import urlIcon from "../../../assets/Icons/UrlIcon.png";
 import calendarIcon from "../../../assets/Icons/CalendarIcon.png";
+import locationIcon from "../../../assets/Icons/LocationIcon.png";
+import remoteIcon from "../../../assets/Icons/RemoteIcon.png";
 
 import { prettifyDate } from "../../../globalHelperFunctions/utils";
 
@@ -101,6 +103,28 @@ export const ApplicationContentContainer = (props) => {
                             <span>Applied</span>
                         </span> 
                         {prettyDate}
+                    </div>
+                    :
+                    null
+                }
+                {props.remote ?
+                    <div>
+                        <span>
+                            <img src={remoteIcon} alt="" />
+                            <span>Remote</span>
+                        </span> 
+                        {props.remote}
+                    </div>
+                    :
+                    null
+                }
+                {props.location ?
+                    <div>
+                        <span>
+                            <img src={locationIcon} alt="" />
+                            <span>Location</span>
+                        </span> 
+                        {props.location}
                     </div>
                     :
                     null
