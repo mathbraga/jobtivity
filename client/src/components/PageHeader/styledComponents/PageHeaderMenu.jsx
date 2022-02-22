729b3d4ea65f448b59cd429202630a6b445e0951
Matheus B <matheus_ob59@hotmail.com>
2022-02-22 18:28:08 -0300

    Create page header menu component.

---
+++ client/src/components/PageHeader/styledComponents/PageHeaderMenu.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const PageHeaderMenu = styled.div`
+    width: auto;
+    height: auto;
+    padding-left: 21px;
+
+    background-color: white;
+
+    position: fixed;
+    left: 0;
+
+    border: 1px solid gold;
+    border-top: none;
+    border-left: none;
+
+    cursor: pointer;
+
+    transform: translateY(-30px);
+    z-index: 1;
+
+    div{
+        margin: 12px 0;
+    }
+`;
--- client/src/components/PageHeader/styledComponents/index.js
+++ client/src/components/PageHeader/styledComponents/index.js
@@ -3,4 +3,5 @@ export { PageHeaderContainer } from "./PageHeaderContainer";
 export { PageHeaderButton } from "./PageHeaderButton";
 export { PageHeaderButtonContainer } from "./PageHeaderButtonContainer";
 export { PageHeaderShadow } from "./PageHeaderShadow";
+export { PageHeaderMenu } from "./PageHeaderMenu";
 export { MenuButton } from "./MenuButton";
