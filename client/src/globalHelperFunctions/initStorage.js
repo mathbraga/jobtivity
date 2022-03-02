b4ada44185a1886de571ede869a85a1244500946
Matheus B <matheus_ob59@hotmail.com>
2022-03-02 18:24:24 -0300

    Include toggle state data to initial local storage items list.

--- client/src/globalHelperFunctions/initStorage.js
+++ client/src/globalHelperFunctions/initStorage.js
@@ -1,11 +1,14 @@
 const storageName = "applicationsList";
 const historyStorageName = "applicationsHistory";
+const toggleStorageName = "toggleState"
 
 const createEmptyStorage = () => {
     if(localStorage.getItem(storageName) === null)
       localStorage.setItem(storageName, "[]");
     if(localStorage.getItem(historyStorageName) === null)
       localStorage.setItem(historyStorageName, "[]");
+    if(localStorage.getItem(toggleStorageName) === null)
+      localStorage.setItem(toggleStorageName, "false");
 }
 
 export default createEmptyStorage;
