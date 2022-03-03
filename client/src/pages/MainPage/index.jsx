d859bee06c3dc41e6da0821f4ba3ea08a43f95cc
Matheus B <matheus_ob59@hotmail.com>
2022-03-03 19:05:59 -0300

    Implement base idea for theme changing in real time.

--- client/src/pages/MainPage/index.jsx
+++ client/src/pages/MainPage/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
 
 import PageHeader from "../../components/PageHeader";
 import Footer from "../../components/Footer";
@@ -7,9 +8,12 @@ import ActiveApplications from "../ActiveApplications";
 import ApplicationsHistory from "../ApplicationsHistory";
 import { MainPageContainer } from "./styledComponents";
 
-const MainPage = () => {
+const MainPage = (props) => {
+    const { themeState } = props.themeState === null ? false : props;
+    const theme = themeState ? "theme--dark" : "";
+
     return(
-        <MainPageContainer>
+        <MainPageContainer className={theme}>
             <PageHeader />
             <Route path="/" exact>
                 <Redirect to="/applications"/>
@@ -21,4 +25,8 @@ const MainPage = () => {
     );
 }
 
-export default MainPage;
+const mapStateToProps = (state) => ({
+    themeState: state.themeToggleReducer.toggleState
+});
+
+export default connect(mapStateToProps, null)(MainPage);
